a6e7130431324ab0d4a7b8fe6f7834862aa97ce5
AI5GW <109100133+AI5GW@users.noreply.github.com>
2022-10-15 20:59:23 +0200

    Update SDS_Oscilloscope_Retrieve_Waveform.py

--- src/SDS_Oscilloscope_Retrieve_Waveform.py
+++ src/SDS_Oscilloscope_Retrieve_Waveform.py
@@ -11,13 +11,19 @@
  *
 """
 
+import sys
 import pyvisa as visa
 import numpy as np
 import matplotlib.pyplot as plotter
 
-#USB resource of Device
-rm = visa.ResourceManager()
-device = rm.open_resource('TCPIP::192.168.188.30::INSTR',query_delay=0.25)
+try:
+    rm = visa.ResourceManager()
+
+    # Connect to device (Make sure to change the resource locator!)
+    device = rm.open_resource('TCPIP::192.168.188.29::INSTR',query_delay=0.25)
+except:
+    print('Failed to connect to device...')
+    sys.exit(0)
 device.timeout = 30000
 
 CHAN = 1
